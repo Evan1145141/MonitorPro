9cfa951e42c7af3446f821e862c15968874cb2bf
Evan1145141 <evan04125@gmail.com>
2025-12-02 18:26:57 +0800

    Updated history.tsx

--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -428,7 +428,7 @@ const styles = StyleSheet.create({
   },
   header: {
     backgroundColor: '#fff',
-    paddingTop: 20,
+    paddingTop: 25,
     paddingBottom: 20,
     paddingHorizontal: 20,
     borderBottomWidth: 1,
